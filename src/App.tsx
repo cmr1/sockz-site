73d71d60c550162383d7fad2dcae82ccbf12e281
Charlie McClung <charlie@cmr1.com>
2022-03-05 02:51:31 -0700

    Deep link down to top of readme on github

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://github.com/cmr1/ts-sockz"
+          href="https://github.com/cmr1/ts-sockz#ts-sockz"
           target="_blank"
           rel="noopener noreferrer"
         >
