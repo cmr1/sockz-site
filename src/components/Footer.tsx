7a85c812a8c524c4d5b22f754d1a2a42a3936f8b
Charlie McClung <charlie@bowtie.co>
2022-03-12 05:27:49 -0700

    Fix back link to use env url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Footer.scss';
 
+const { REACT_APP_SOCKZ_HOST } = process.env;
+
 const Footer = ({ auth, authorized, updateAuth }) => {
   return (
     <div className="Footer">
@@ -8,7 +10,7 @@ const Footer = ({ auth, authorized, updateAuth }) => {
         {/* {auth && authorized && <Button color='warning' onClick={() => updateAuth(null)}>Logout</Button>} */}
         <a
           className="App-link"
-          href="https://localhost:4040"
+          href={`https://${REACT_APP_SOCKZ_HOST}`}
         >
           &lt; back to dashboard
         </a>
